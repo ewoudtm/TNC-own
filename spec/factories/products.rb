7fd55d2957b7394e0528f5d94a5cdc7ab8043666
Awadje <michaelawad85@gmail.com>
2017-04-24 21:19:21 +0200

    Added/Amend factories, user model test product relation

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :product do
     title "MyString"
     start_price 1
+    image "testimage.jpg"
+    description "Test"
+    size "L"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user1) { create :user }
+  let(:product1) { create :product, user: user1 }
+
+
+    describe "user knows about products" do
+      it "knows the day he's in" do
+        expect(user1.products).to include(product1)
+
+      end
+    end
 end

a8adbc7db742f8fba1af39dcbd8732b6eda37b90
Winkeltje84 <bwinkel@gmail.com>
2017-04-24 17:59:34 +0200

    added to Product Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424150917) do
+ActiveRecord::Schema.define(version: 20170424155559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,15 @@ ActiveRecord::Schema.define(version: 20170424150917) do
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.integer  "start_price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "image"
+    t.string   "condition"
+    t.text     "description"
+    t.string   "size"
+    t.boolean  "sold",        default: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +48,5 @@ ActiveRecord::Schema.define(version: 20170424150917) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "products", "users"
 end

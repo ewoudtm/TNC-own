b65ab7789a63d07375b64a83de666bf68bbf55a2
Awadje <michaelawad85@gmail.com>
2017-04-24 22:05:50 +0200

    Updated Factories and User Product_negotiation relation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user1) { create :user }
+  let(:user2) { create :user }
   let(:product1) { create :product, user: user1 }
+  let(:product_negotiation1) { create :product_negotiation, users_id: user2 }
+
 
 
     describe "user knows about products" do
-      it "knows the day he's in" do
+      it "knows the product he's in" do
         expect(user1.products).to include(product1)
 
       end
     end
+
+    describe "user knows about product negotiation" do
+      it "knows the product negotiation he's in" do
+        expect(user2.product_negotiations).to include(product_negotiation1)
+
+      end
+    end
+
 end

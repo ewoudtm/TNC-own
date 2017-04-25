76af58a944d6b7f5162c11d205fc2a1365222d6f
ewoudtm <ewoud@ewoudtm.nl>
2017-04-25 17:37:01 +0200

    new seeds and added attributes to product model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,67 +2,165 @@ User.delete_all
 Product.delete_all
 
 user1 = User.create!(
-  first_name: "Angela",
-  last_name: "Merkel",
-  email: "angela@merkel.com",
-  password: "abc123"
+  first_name: "Evy",
+  last_name: "Mennens",
+  email: "evytnc@gmail.com",
+  password: "@bcd1234"
 )
 user2 = User.create!(
-  first_name: "Bob",
-  last_name: "de Jong",
-  email: "bob@dejong.com",
-  password: "abc123"
+  first_name: "Sjeel",
+  last_name: "Korenromp",
+  email: "sjeeltnc@gmail.com",
+  password: "@bcd1234"
 )
 user3 = User.create!(
-  first_name: "Sarah",
-  last_name: "de Rooij",
-  email: "sarah@derooij.com",
-  password: "abc123"
+  first_name: "ILJA",
+  last_name: "Visser",
+  email: "iljatnc@gmail.com",
+  password: "@bcd1234"
 )
 
 
 product1 = Product.create(
-  title: "Rok Day Birger & Mikelsen",
-  start_price: 25,
-  image: "https://cdna.lystit.com/photos/1298-2015/06/14/jimmy-choo-grey-agnes-pumps-gray-product-1-055664419-normal.jpeg",
+  title: "GIVENCHY",
+  product_type: "Enkellaarzen",
+  original_price: 1315,
+  start_price: 900,
+  image: "https://thenextcloset.com/system/uploads/asset/file/426914/large_image.jpg",
   condition: "Kleine gebruikssporen",
-  description: "Gedragen maar kan zeer zeker nog mooie rondes mee",
-  size: "M",
-  user_id: user1
+  description: "Givenchy 'TRIA' boots. Maar 3x gedragen. Aangekocht februari 2016 In de Bijenkorf in Amsterdam. Originele prijs €1315,00.\nMateriaal: Suede\nKleur: Beige",
+  size: "38",
+  user_id: user1.id
 )
 product2 = Product.create(
-  title: "Jimmy Choo Pumps",
-  start_price: 575,
-  image: "https://cdn.rebelle.com/20/2014-07-0820140708-12988-1qh8w57.jpg?width=420&height=420",
-  condition: "Nieuw",
-  description: "Prachtige nieuw in doos Jimmy Choo Aude Cubed Nitro leather pump in zilver kleur. Gekocht voor een gelegenheid maar niet gebruikt daarvoor. Een keer gepast en binnen even op gelopen maar daarna altijd in de kast blijven staan. Prachtige pump klassiek en tijdloos en zeer edgy en trendy nu voor een geweldige prijs.",
-  size: "39",
-  user_id: user1
+  title: "PATRIZIA PEPE",
+  product_type: "Poncho",
+  original_price: 240,
+  start_price: 115,
+  image: "https://thenextcloset.com/system/uploads/asset/file/423921/large_18-4__76_.JPG",
+  condition: "Topconditie",
+  description: "Grijs/Taupe kleur Poncho van Patrizia Pepe. Maat 1, denk dat dit overeenkomt met maat 36, maar ook geschikt voor een maat groter of kleiner. Kan zo gedragen worden of bijvoorbeeld over een leren jasje. Met grote kol.\nGebruikte materialen: Acryl, Alpaca, wol.",
+  size: "36",
+  user_id: user2.id
 )
 product3 = Product.create(
-  title: "Bellerose Jas",
-  start_price: 65,
-  image: "https://www.orangebag.nl/cache/bellerose-jassen.303644/bellerose-jassen-1728-2304.jpg",
-  condition: "Kleine gebruikssporen",
-  description: "Lekker Bellrose jasje, met dunne warme voering.",
-  size: "S",
-  user_id: user2
+  title: "ILJA VISSER",
+  product_type: "Top",
+  original_price: 199,
+  start_price: 55,
+  image: "https://thenextcloset.com/system/uploads/asset/file/440561/large_25-4__16_.jpg",
+  condition: "Nieuw",
+  description: "Prachtige exclusieve witte nieuwe blouse van de modeontwerpster ILJA VISSER\nMaat 36\nZijde\nNieuw met kaartje 199",
+  size: "36",
+  user_id: user1.id
 )
 product4 = Product.create(
-  title: "Swarovski Zonnebril",
-  start_price: 550,
-  image: "https://www.perssupport.nl/uploads/cache/press_news_item_og/uploads/press-news-item/original/3a7effe7-092f-11e1-9432-8b7e440500ec.jpg",
-  condition: "Zo goed als nieuw",
-  description: "collectors item zonnebril van Swarovski. beperkte oplage.Deze zonnebril heeft rond de 1200 euro gekost. De zijkant zitten de mooie Swarovski stenen",
-  size: "One size fits all",
-  user_id: user2
+  title: "STILLS",
+  product_type: "Jurk",
+  original_price: 299,
+  start_price: 79,
+  image: "https://thenextcloset.com/system/uploads/asset/file/440476/large_25-4__53_.jpg",
+  condition: "Nieuw",
+  description: "Prachtige mooie nieuwe jurk van STILLS in maat 36\nDonker mais geel afgezet met een bruin randje\nGevoerd\nNieuw met kaartje 299",
+  size: "36",
+  user_id: user1.id
 )
 product5 = Product.create(
-  title: "Coach Schoudertas",
-  start_price: 785,
-  image: "https://cdn.rebelle.com/20/2014-07-0820140708-12988-1qh8w57.jpg?width=420&height=420",
-  condition: "Binnenvoering heeft kleine rafel",
-  description: "Royale tas van Coach. Met mooie details en 3 ruime vakken, dubbele handgrepen en of verwijderbare schouderriempjes. Het is gemaakt van kiezelleer met een suede interieur. Afm 26x31x14cm.",
-  size: "30 bij 40 cm",
-  user_id: user1
+  title: "PRADA",
+  product_type: "Shopper",
+  original_price: 900,
+  start_price: 450,
+  image: "https://thenextcloset.com/system/uploads/asset/file/439700/large_25-4__23_.jpg",
+  condition: "1x gedragen",
+  description: "Prada shopper van zeer soepel maar stevig leer. Nieuw.\nMateriaal: Leer\nKleur: Blauw ",
+  size: "",
+  user_id: user2.id
+)
+product6 = Product.create(
+  title: "CHRISTIAN LOUBOUTIN",
+  product_type: "Enkellaarzen",
+  original_price: 895,
+  start_price: 295,
+  image: "https://thenextcloset.com/system/uploads/asset/file/440169/large_25-4__63_.JPG",
+  condition: "Nieuw",
+  description: "Christian Louboutin enkellaarsjes 'Fastwist 140' in bruin suede. Met 140mm behoorlijk hoog, maar er zit ruim 2 cm platform in waardoor ze toch minder hoog voelen.\nVallen naar maat (zijn mij met mijn maat 39 dus helaas te groot).\nNieuw en ongedragen inclusief originele doos.\nMateriaal: Suede \nKleur: Bruin ",
+  size: "40",
+  user_id: user3.id
+)
+product7 = Product.create(
+  title: "ASH",
+  product_type: "Sneakers",
+  original_price: 175,
+  start_price: 50,
+  image: "https://thenextcloset.com/system/uploads/asset/file/429902/large_20-4__168_.JPG",
+  condition: "",
+  description: "Stoere, trendy sneakers in kaki camouflage print van het hippe merk ASH. Ze lopen heerlijk en zijn helemaal hot. De witte neus is opzettelijk enigszins oud gemaakt, lijkt langs de randen wat 'groezelig', maar zo zijn ze gemaakt, zaten zo in de doos, ze zijn slechts een paar keer gedragen en zien er, afgezien van wat gebruikssporen op de zolen, perfect uit. Een aanrader. De maat is 37. Neem een kijkje in mijn boutique, bijna dagelijks nieuwe items van top merken.",
+  size: "37",
+  user_id: user1.id
+)
+product8 = Product.create(
+  title: "CITIZENS OF HUMANITY",
+  product_type: "Jeans",
+  original_price: 225,
+  start_price: 40,
+  image: "https://thenextcloset.com/system/uploads/asset/file/440754/large_25-4__17_.jpg",
+  condition: "Topconditie",
+  description: "Prachtige jeans van Citizens of Humanity, Avedon. Maat 28. Met 2% elastan voor de perfecte pasvorm.\nMateriaal: Denim\nKleur: Blauw ",
+  size: "28",
+  user_id: user3.id
+)
+product9 = Product.create(
+  title: "LOUIS VUITTON",
+  product_type: "Enkellaarzen",
+  original_price: 1095,
+  start_price: 185,
+  image: "https://thenextcloset.com/system/uploads/asset/file/433685/large_2041608-1_1_1_.jpg",
+  condition: "Topconditie",
+  description: "Mooie zomerlaarsjes in leder van Louis Vuitton\nHakhoogte : 12 cm \nHoogte van de laars : 30 cm \nHak in hout en sluiting met rits aan de achterzijde\nMateriaal: Leer \nKleur: Beige ",
+  size: "39",
+  user_id: user1.id
+)
+product10 = Product.create(
+  title: "TWIN-SET",
+  product_type: "Top",
+  original_price: 215,
+  start_price: 40,
+  image: "https://thenextcloset.com/system/uploads/asset/file/434375/large_P1060097.JPG",
+  condition: "",
+  description: "Mooie gehaakte tuniek van Twin Set met langere ondertop met verstelbare bandjes. Een paar maal gedragen.\nMateriaal: Katoen\nKleur: Beige ",
+  size: "36",
+  user_id: user2.id
+)
+product11 = Product.create(
+  title: "IRO",
+  product_type: "Jas",
+  original_price: 540,
+  start_price: 150,
+  image: "https://thenextcloset.com/system/uploads/asset/file/439660/large_25-4__105_.JPG",
+  condition: "Topconditie",
+  description: "IRO's Hoover Jacket. Black jacket with leather sleeves | Assymetric zipper flap front | Zip pockets | Leather trim at hems. 52% Cotton | 44% Linen | 3% Polyamide, 1% Elastane. Dry Clean. Mt 3\nMateriaal: Katoen \nKleur: Zwart ",
+  size: "38",
+  user_id: user3.id
+)
+product12 = Product.create(
+  title: "NIKE",
+  product_type: "Sneakers",
+  original_price: 100,
+  start_price: 40,
+  image: "https://thenextcloset.com/system/uploads/asset/file/411478/large_20170405_120051.jpg",
+  condition: "Topconditie",
+  description: "Nike internationalist schoenen in hele goede conditie, slechts paar keer gedragen. De schoenen zijn deels van suede, deels mesh-achtig materiaal (want het zijn sneakers). Maat valt echt als 36,5 (US 6; UK 3.,5; EUR 36,5; cm 23). Inclusief doos.\nMateriaal: Suede \nKleur: Blauw",
+  size: "36,5",
+  user_id: user1.id
+)
+product13 = Product.create(
+  title: "ARMANI",
+  product_type: "Jeans",
+  original_price: 169,
+  start_price: 45,
+  image: "https://thenextcloset.com/system/uploads/asset/file/363152/large_FullSizeRender-27.jpg",
+  condition: "Topconditie",
+  description: "Mooie Armani jeans, laag model. Weinig gedragen. Te klein gekocht.\nMateriaal: Katoen \nKleur: Blauw ",
+  size: "36",
+  user_id: user1.id
 )

28710969ec5ee29f8d17f8a47bc52c6b73a4e930
Winkeltje84 <bwinkel@gmail.com>
2017-04-25 22:03:12 +0200

    Authorization of Buyers and Sellers in Productnegotiations Controller. STILL NEEDS TO BE TRANSFERRED TO MODEL

--- app/controllers/productnegotiations_controller.rb
+++ app/controllers/productnegotiations_controller.rb
@@ -1,13 +1,14 @@
 class ProductnegotiationsController < ApplicationController
   before_action :set_product_negotiation, only: [:show]
   before_action :set_single_bids, only: [:show]
+  before_action :authorize_buyer_seller, only: [:show]
 
 
   def index
   end
 
   def show
-    
+
 
     @current_negotiation_bids = ProductNegotiation.get_bids_from_current_negotiation(@single_bids, params)
 
@@ -35,6 +36,15 @@ class ProductnegotiationsController < ApplicationController
 
     def set_single_bids
       @single_bids = SingleBid.all
+    end
+
+    def authorize_buyer_seller
+      # if current_user id is equal to Product owner or Product bidder, ok to continue, otherwise not.
+      if (current_user.id == @product_negotiation.user.id) || (current_user.id == @product_negotiation.product.user_id)
+        return
+      else
+        redirect_to product_path, notice: "Sorry, this is not your negotiation"
+      end
     end
 
 

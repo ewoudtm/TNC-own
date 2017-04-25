c82e4647b4cc67e427cd5543ee9dfa6746afe01d
Awadje <michaelawad85@gmail.com>
2017-04-25 12:17:06 +0200

    PN test now passing

--- spec/factories/single_bids.rb
+++ spec/factories/single_bids.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :single_bid do
-    product_negotiation { build (:product_negotiation) }
+    # product_negotiation { build (:product_negotiation) }
     price 1
     counter_offer false
     accepted false
--- spec/models/product_negotiation_spec.rb
+++ spec/models/product_negotiation_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
-
 RSpec.describe ProductNegotiation, type: :model do
-
+    let(:user2) { create :user }
+    let(:product_negotiation1) { create :product_negotiation, user: user2 }
+      describe "Product negotiation relations" do
+        it "has a user" do
+          expect(product_negotiation1.user).to eq(user2)
+        end
+      end
 end

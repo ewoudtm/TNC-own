1e159497ee6695b682e02ec40451d17b06cbd79b
Winkeltje84 <bwinkel@gmail.com>
2017-04-26 20:03:18 +0200

    fixed problems with redirect

--- app/controllers/productnegotiations_controller.rb
+++ app/controllers/productnegotiations_controller.rb
@@ -16,10 +16,12 @@ class ProductnegotiationsController < ApplicationController
   def new
     redirect_if_negotiation_already_exists
 
-    product_negotiation = ProductNegotiation.new
-    product_negotiation.update_attributes(product_id: params[:format], user_id: current_user.id)
-    product_negotiation.save
-    redirect_to productnegotiation_path(product_negotiation.id)
+    if (@already_redirected == false)
+      product_negotiation = ProductNegotiation.new
+      product_negotiation.update_attributes(product_id: params[:format], user_id: current_user.id)
+      product_negotiation.save
+      redirect_to productnegotiation_path(product_negotiation.id), notice: "Succesfully created a new negotiation"
+    end
   end
 
   def create
@@ -52,9 +54,11 @@ class ProductnegotiationsController < ApplicationController
 
     def redirect_if_negotiation_already_exists
       # redirect to negotiation if Buyer already has a negotiation for this product
+      @already_redirected = false
       found_negotiation = current_user.product_negotiations.find do |negotiation|
         negotiation.product_id == params[:format].to_i
       end
       redirect_to productnegotiation_path(found_negotiation.id), notice: "You already have a negotiation for this product" unless found_negotiation == nil
+      @already_redirected = true unless found_negotiation == nil
     end
 end

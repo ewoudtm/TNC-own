1a9fb6b570b6f344a10511b0137ad8bc98521bfa
Awadje <michaelawad85@gmail.com>
2017-04-28 10:58:48 +0200

    Max amount of bids per negotiation set to 4, notice and redirect included

--- app/controllers/singlebids_controller.rb
+++ app/controllers/singlebids_controller.rb
@@ -16,8 +16,13 @@ class SinglebidsController < ApplicationController
   end
 
   def create
+    bits = ProductNegotiation.find(params[:productnegotiation_id].to_i).single_bids
+
+    if (bits.length <= 4)
       @newbid = SingleBid.create!(product_negotiation_id: params[:productnegotiation_id], price: params[:price], user_id: current_user.id)
        redirect_to productnegotiation_path(params[:productnegotiation_id])
+     end
+   else redirect_to productnegotiation_path(params[:productnegotiation_id]), notice: "Max amount of bids reached"
   end
 
   def update

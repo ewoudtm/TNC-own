ed609ab009f7206f0cb86f6879ffd55d75dcb012
Dvessen83 <d.v.essen@gmail.com>
2017-04-28 14:40:29 +0200

    Laatste updates

--- app/controllers/singlebids_controller.rb
+++ app/controllers/singlebids_controller.rb
@@ -19,12 +19,13 @@ class SinglebidsController < ApplicationController
     min_price = 20
     seventy_percent_startprice = 0.7 * ProductNegotiation.find(params[:productnegotiation_id].to_i).product.start_price
 
-
-    if (params[:price].to_i < min_price)
-      redirect_to productnegotiation_path(params[:productnegotiation_id]), alert: "Minumum offer must be at least 20 euros"
+    if (bits.length >= 4)
+      redirect_to productnegotiation_path(params[:productnegotiation_id]), alert: "Maximum amount of offers reached"
     elsif (params[:price].to_i < seventy_percent_startprice)
-      redirect_to productnegotiation_path(params[:productnegotiation_id]), alert: "Minumum amount of the offer has to be at least 70% of the product price"
-    elsif (bits.length <= 4)
+      redirect_to productnegotiation_path(params[:productnegotiation_id]), alert: "Minimum amount of the offer has to be at least 70% of the product price"
+    elsif (params[:price].to_i < min_price)
+      redirect_to productnegotiation_path(params[:productnegotiation_id]), alert: "Minimum offer must be at least 20 euros"
+    else
       @newbid = SingleBid.create!(product_negotiation_id: params[:productnegotiation_id], price: params[:price], user_id: current_user.id)
       if @newbid.save
         product_negotiation = ProductNegotiation.find(@newbid.product_negotiation_id)
@@ -33,9 +34,8 @@ class SinglebidsController < ApplicationController
         AuctionMailer.new_offer_email(buyer, product_negotiation, @newbid).deliver_now
         AuctionMailer.new_offer_email(seller, product_negotiation, @newbid).deliver_now
         redirect_to productnegotiation_path(params[:productnegotiation_id]), notice: "Offer created"
-      else
-        render :new
       end
+    end
   end
 
   def update
